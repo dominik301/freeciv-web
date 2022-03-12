c72d2a1508d8bf52cacdb44ae752258988a4c29d
Dominik <dhuesener@gmail.com>
2022-03-12 10:31:47 +0100

    remove code for play by email

--- freeciv-web/src/main/webapp/static/javascript/index.js
+++ freeciv-web/src/main/webapp/static/javascript/index.js
@@ -13,32 +13,6 @@ $(document).ready(function () {
 		});
 	
 		function loadBestOfPlayByEmail() {
-		
-			var clearContent = function () {
-				document.getElementById('best-of-play-by-email').style.display = 'none';
-			};
-		
-			$.getJSON('/game/play-by-email/top', function(data) {
-				if (data.length === 0) {
-					clearContent();
-				}
-				var root = document.getElementById('play-by-email-list');
-				data.forEach(function (item, i) {
-					var row = document.createElement('tr');
-					var rank = document.createElement('td');
-					var player = document.createElement('td');
-					var wins = document.createElement('td');
-				
-					rank.innerHTML = "#" + (i + 1);
-					player.innerHTML = item.player;
-					wins.innerHTML = item.wins;
-				
-					row.appendChild(rank);
-					row.appendChild(player);
-					row.appendChild(wins);
-					root.appendChild(row);
-				});
-			}).fail(clearContent);
 		}
 
 		function displayStore() {
@@ -56,6 +30,6 @@ $(document).ready(function () {
 
 if ('serviceWorker' in navigator) {
 	navigator.serviceWorker
-		.register('/static/javascript/sw.js')
+		.register('/static/sw.js')
 		.then(() => {console.log('Service Worker Registered'); });
 }
